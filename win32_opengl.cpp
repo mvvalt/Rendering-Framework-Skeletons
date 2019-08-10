717c7c9bda4acbd500ab8479be5d690e13148e64
mvvalt <30536894+mvvalt@users.noreply.github.com>
2019-08-10 16:52:35 -0500

    Update win32_opengl.cpp
    Added support for Alt+Enter.

--- win32_opengl.cpp
+++ win32_opengl.cpp
@@ -11,38 +11,39 @@ PFNWGLCHOOSEPIXELFORMATARBPROC wglChoosePixelFormatARB = nullptr;
 PFNWGLCREATECONTEXTATTRIBSARBPROC wglCreateContextAttribsARB = nullptr;
 
 
+struct WindowData
+{
+	HWND hwnd;
+	HDC hdc;
+	HGLRC hglrc;
+};
+
+struct ConfigOptions
+{
+	// Window
+	bool fullscreen;
+	int monitor_width;
+	int monitor_height;
+	int display_width;
+	int display_height;
+	
+	int multisample_samples;
+	bool vsync;
+
+	// Misc
+	float update_interval;
+};
+
+
+WindowData g_window_data = {};
+ConfigOptions g_config = {};
 LARGE_INTEGER g_performance_frequency = {};
 
 
-bool running = true;
+bool g_running = true;
 
 
-// A critical error caused by one of the operating system calls failing
-void system_error(const char *msg)
-{
-	MessageBoxEx(NULL, msg, TEXT("System Error"), MB_OK | MB_ICONERROR, 0);
-	// @TODO: log to file
-}
-
-
-// An opengl error
-void APIENTRY opengl_error(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar *message, const void *user_param)
-{
-	source;
-	type;
-	id;
-	severity;
-	length;
-	//message;
-	user_param;
-
-	MessageBoxEx(NULL, (char *)message, TEXT("Opengl Error"), MB_OK | MB_ICONERROR, 0);
-	// @TODO: log to file
-}
-
-
-// @NOTE: This is not a critical operation, so we just get the
-// function pointer on demand.
+// @NOTE: This is not a critical operation, so we just get the function pointer on demand.
 void toggle_vsync(bool enabled)
 {
 	typedef BOOL(APIENTRY *PFNWGLSWAPINTERVALPROC)(int);
@@ -62,6 +63,76 @@ void toggle_vsync(bool enabled)
 }
 
 
+void toggle_window(bool fullscreen)
+{
+	if (fullscreen)
+	{
+		SetWindowLongPtr(g_window_data.hwnd, GWL_STYLE, WS_POPUP | WS_VISIBLE);
+		MoveWindow(g_window_data.hwnd, 0, 0, g_config.monitor_width, g_config.monitor_height, FALSE);
+	}
+	else
+	{
+		DWORD style = WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_VISIBLE; // No min/max buttons
+		//style |= WS_THICKFRAME; // Resizing the window manually is allowed
+
+		SetWindowLongPtr(g_window_data.hwnd, GWL_STYLE, style);
+		RECT window_rect = { 0, 0, g_config.display_width, g_config.display_height };
+		AdjustWindowRectEx(&window_rect, style, FALSE, 0);
+		int wx = (g_config.monitor_width - (window_rect.right - window_rect.left)) / 2;
+		int wy = (g_config.monitor_height - (window_rect.bottom - window_rect.top)) / 2;
+		MoveWindow(g_window_data.hwnd, wx, wy, window_rect.right - window_rect.left, window_rect.bottom - window_rect.top, TRUE);
+		InvalidateRect(NULL, NULL, TRUE); // Necessary to redraw the desktop
+	}
+
+	// This fixes a strange issue where the taskbar icon will not show up for this window
+	// when using Alt+Enter
+	SetActiveWindow(g_window_data.hwnd);
+
+	// @TODO: notify renderer
+}
+
+
+// A critical error caused by one of the operating system calls failing
+void system_error(const char *msg)
+{
+	// @TODO: log to file
+	MessageBoxEx(NULL, msg, TEXT("System Error"), MB_OK | MB_ICONERROR, 0);
+	abort();
+}
+
+
+// An opengl error
+void APIENTRY opengl_error(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar *message, const void *user_param)
+{
+	source;
+	type;
+	id;
+	severity;
+	length;
+	//message;
+	user_param;
+
+	switch (severity)
+	{
+		case GL_DEBUG_SEVERITY_HIGH:
+		{
+			// @TODO: error
+		} break;
+
+		case GL_DEBUG_SEVERITY_MEDIUM: // Fallthrough
+		case GL_DEBUG_SEVERITY_LOW:
+		{
+			// @TODO: warning
+		} break;
+
+		case GL_DEBUG_SEVERITY_NOTIFICATION:
+		{
+			// Do nothing?
+		} break;
+	}
+}
+
+
 float elapsed_time(LARGE_INTEGER start, LARGE_INTEGER end)
 {
 	return (float)((end.QuadPart - start.QuadPart) / (double)g_performance_frequency.QuadPart);
@@ -73,6 +144,19 @@ LRESULT CALLBACK window_procedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lpa
 	LRESULT result = 0;
 	switch (msg)
 	{
+		case WM_ACTIVATE: // 0
+		{
+			if ((wparam == WA_ACTIVE) || (wparam == WA_CLICKACTIVE))
+			{
+				OutputDebugString("activated");
+			}
+			else if (wparam == WA_INACTIVE)
+			{
+				OutputDebugString("deactivated");
+				// @TODO: pause?
+			}
+		} break;
+
 		case WM_CREATE: // 0
 		{
 		} break;
@@ -94,6 +178,27 @@ LRESULT CALLBACK window_procedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lpa
 			//renderer::window_resize(LOWORD(lparam), HIWORD(lparam));
 		} break;
 
+		case WM_SYSKEYDOWN: // 0
+		{
+			if (wparam == VK_RETURN)
+			{
+				if ((lparam & (1 << 30)) == 0)
+				{
+					g_config.fullscreen = !g_config.fullscreen;
+					toggle_window(g_config.fullscreen);
+				}
+			}
+		} break;
+
+		case WM_KEYDOWN: // 0
+		{
+			// @DEVELOPMENT
+			if (wparam == VK_ESCAPE)
+			{
+				g_running = false;
+			}
+		} break;
+
 		default:
 		{
 			result = DefWindowProc(hwnd, msg, wparam, lparam);
@@ -104,31 +209,7 @@ LRESULT CALLBACK window_procedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lpa
 }
 
 
-struct WindowData
-{
-	HWND hwnd;
-	HDC hdc;
-	HGLRC hglrc;
-};
-
-
-struct ConfigOptions
-{
-	// Window
-	bool fullscreen; // Do we run the game fullscreen at the desktop resolution?
-	int fullscreen_width;
-	int fullscreen_height;
-	int windowed_width;
-	int windowed_height;
-	int multisample_samples;
-	bool vsync;
-
-	// Misc
-	float update_interval;
-};
-
-
-bool create_window(HINSTANCE hinstance, ConfigOptions &config, WindowData &wnd_data)
+bool create_window(HINSTANCE hinstance, WindowData &wnd_data)
 {
 	bool result = false;
 
@@ -147,30 +228,7 @@ bool create_window(HINSTANCE hinstance, ConfigOptions &config, WindowData &wnd_d
 	wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
 	if (RegisterClassEx(&wc))
 	{
-		DWORD style = 0;
-		RECT window_rect;
-		int primary_screen_width = GetSystemMetrics(SM_CXSCREEN);
-		int primary_screen_height = GetSystemMetrics(SM_CYSCREEN);
-		int wx = 0;
-		int wy = 0;
-		if (config.fullscreen)
-		{
-			config.fullscreen_width = primary_screen_width;
-			config.fullscreen_height = primary_screen_height;
-			style = WS_POPUP;
-			window_rect = { 0, 0, config.fullscreen_width, config.fullscreen_height };
-		}
-		else
-		{
-			style = WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU; // No min/max buttons
-			//style |= WS_THICKFRAME; // Resizing the window manually is allowed
-			window_rect = { 0, 0, config.windowed_width, config.windowed_height };
-			AdjustWindowRectEx(&window_rect, style, FALSE, 0);
-			wx = (primary_screen_width - (window_rect.right - window_rect.left)) / 2;
-			wy = (primary_screen_height - (window_rect.bottom - window_rect.top)) / 2;
-		}
-
-		wnd_data.hwnd = CreateWindowEx(0, wc.lpszClassName, "Demo", style, wx, wy, window_rect.right - window_rect.left, window_rect.bottom - window_rect.top, NULL, NULL, hinstance, NULL);
+		wnd_data.hwnd = CreateWindowEx(0, wc.lpszClassName, "Demo", WS_OVERLAPPEDWINDOW, 0, 0, 1, 1, NULL, NULL, hinstance, NULL);
 		if (wnd_data.hwnd)
 		{
 			wnd_data.hdc = GetDC(wnd_data.hwnd);
@@ -187,7 +245,7 @@ bool create_window(HINSTANCE hinstance, ConfigOptions &config, WindowData &wnd_d
 				WGL_DEPTH_BITS_ARB, 24,
 				WGL_STENCIL_BITS_ARB, 8,
 				WGL_SAMPLE_BUFFERS_ARB, 1,			// multisampling enabled
-				WGL_SAMPLES_ARB, config.multisample_samples,	// multisampling samples (e.g. 4x, 8x)
+				WGL_SAMPLES_ARB, g_config.multisample_samples,	// multisampling samples (e.g. 4x, 8x)
 				0
 			};
 			int pf_id;
@@ -317,7 +375,7 @@ bool create_temporary_window(HINSTANCE hinstance, WindowData &wnd_data)
 
 	return result;
 }
-bool initialize_window(HINSTANCE hinstance, ConfigOptions &config, WindowData &wnd_data)
+bool initialize_window(HINSTANCE hinstance, WindowData &wnd_data)
 {
 	bool result = false;
 
@@ -327,7 +385,7 @@ bool initialize_window(HINSTANCE hinstance, ConfigOptions &config, WindowData &w
 	if (create_temporary_window(hinstance, temp_wnd_data))
 	{
 		// Create the real window and opengl context
-		if (create_window(hinstance, config, wnd_data))
+		if (create_window(hinstance, wnd_data))
 		{
 			wglMakeCurrent(NULL, NULL);
 			wglDeleteContext(temp_wnd_data.hglrc);
@@ -339,7 +397,7 @@ bool initialize_window(HINSTANCE hinstance, ConfigOptions &config, WindowData &w
 				gladLoadGL();
 
 				// Set vsync
-				toggle_vsync(config.vsync);
+				toggle_vsync(g_config.vsync);
 
 				// Opengl debug callback
 				glDebugMessageCallback(opengl_error, nullptr);
@@ -372,41 +430,32 @@ int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE, LPSTR lpcmdline, int)
 
 
 	// @TODO: load config from file
-	ConfigOptions config = {};
-	config.fullscreen = false;
-	config.fullscreen_width = config.fullscreen_height = -1; // These are set to the desktop resolution
-	config.windowed_width = 1280;
-	config.windowed_height = 720;
-	config.multisample_samples = 4;
-	config.vsync = true;
-	config.update_interval = 1.0f / 30.0f;
+	g_config.fullscreen = true;
+	g_config.monitor_width = GetSystemMetrics(SM_CXSCREEN); // No user override
+	g_config.monitor_height = GetSystemMetrics(SM_CYSCREEN); // No user override
+	g_config.display_width = 1600;
+	g_config.display_height = 900;
+	g_config.multisample_samples = 4;
+	g_config.vsync = true;
+	g_config.update_interval = 1.0f / 60.0f;
 
 
 	// @TODO: command line
 	lpcmdline;
 
-
-	WindowData window_data = {};
-	if (initialize_window(hinstance, config, window_data))
+		
+	if (initialize_window(hinstance, g_window_data))
 	{
-
 #ifdef GD_LOGTOCONSOLE
 		AllocConsole();
 		freopen_s((FILE **)stdout, "CONOUT$", "w", stdout);
 #endif
 
-		if (config.fullscreen)
-		{
-			//renderer::initialize(config.fullscreen_width, config.fullscreen_height);
-		}
-		else
-		{
-			//renderer::initialize(config.windowed_width, config.windowed_height);
-		}
-
+		//renderer::initialize(config.display_width, config.display_height);
+		
 
 		// @TEMP: Show us what version of opengl we're using
-		SetWindowText(window_data.hwnd, (LPCSTR)glGetString(GL_VERSION));
+		SetWindowText(g_window_data.hwnd, (LPCSTR)glGetString(GL_VERSION));
 
 
 		QueryPerformanceFrequency(&g_performance_frequency);
@@ -420,20 +469,20 @@ int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE, LPSTR lpcmdline, int)
 		QueryPerformanceCounter(&update_timer_start);
 		QueryPerformanceCounter(&per_second_timer_start);
 
-		ShowWindow(window_data.hwnd, SW_SHOW);
+		toggle_window(g_config.fullscreen);
 		result = 0;
 
-		while (running)
+		while (g_running)
 		{
 			MSG msg;
-			while (PeekMessage(&msg, window_data.hwnd, 0, 0, PM_REMOVE))
+			while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
 			{
 				TranslateMessage(&msg);
 				DispatchMessage(&msg);
 
 				if (msg.message == WM_QUIT)
 				{
-					running = false;
+					g_running = false;
 				}
 			}
 
@@ -442,20 +491,22 @@ int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE, LPSTR lpcmdline, int)
 			LARGE_INTEGER update_timer_now;
 			QueryPerformanceCounter(&update_timer_now);
 			update_accumulator += elapsed_time(update_timer_start, update_timer_now);
-			while (update_accumulator >= config.update_interval)
+			while (update_accumulator >= g_config.update_interval)
 			{
 				++update_counter;
-				update_accumulator -= config.update_interval;
+				update_accumulator -= g_config.update_interval;
 
-				// @TODO: application_update(config.update_interval);
+				// @TODO: update(config.update_interval);
 			}
 			update_timer_start = update_timer_now;
 
 
 			// Render
 			//renderer::draw();
+			glClearColor(0.0f, 0.3f, 0.3f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
 
-			SwapBuffers(window_data.hdc);
+			SwapBuffers(g_window_data.hdc);
 			++render_counter;
 
 
@@ -466,7 +517,7 @@ int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE, LPSTR lpcmdline, int)
 			{
 				char buf[0x100];
 				wsprintf(buf, "UPS: %d, FPS: %d", update_counter, render_counter);
-				SetWindowText(window_data.hwnd, buf);
+				SetWindowText(g_window_data.hwnd, buf);
 
 				update_counter = 0;
 				render_counter = 0;
